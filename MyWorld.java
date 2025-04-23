40abcca3c57c2be5a55b18042ccfade0fe0acf78
AdamFungus <afsomerset@gmail.com>
2025-04-22 23:06:24 -0400

    add enemy

--- MyWorld.java
+++ MyWorld.java
@@ -8,5 +8,8 @@ public class MyWorld extends World
         
         Hero hero = new Hero();
         addObject(hero, 100, 100);
+        
+        Enemy enemy = new Enemy();
+        addObject(enemy, 600, 100);
     }
 }
