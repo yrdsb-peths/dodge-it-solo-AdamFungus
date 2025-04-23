11a2c45c03a85b3ab4abd9c247adb761797a5a94
AdamFungus <afsomerset@gmail.com>
2025-04-22 23:11:03 -0400

    Enemy randomizer

--- Enemy.java
+++ Enemy.java
@@ -15,6 +15,19 @@ public class Enemy extends Actor
     public void act()
     {
         move(-3);
+        if(getX() <= 0) {
+            resetBanana();
+        }
+        
         // Add your action code here.
+    }
+    public void resetBanana() {
+            int num = Greenfoot.getRandomNumber(2);
+            if(num == 0) {
+                setLocation(600, 100);
+            } else {
+                setLocation(600, 300);
+            }
+
     }
 }
